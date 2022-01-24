e8c3e0b921d1895b6f666729d54bd6e952ec60fd
Svyatoslav <sergeyevsvyatoslav@yandex.ru>
2022-01-25 00:27:27 +0300

    existsAuthorByUsername() added

--- src/main/java/tech/sergeyev/linkshortener/persistence/repository/AuthorRepository.java
+++ src/main/java/tech/sergeyev/linkshortener/persistence/repository/AuthorRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import tech.sergeyev.linkshortener.persistence.model.Author;
 
+import java.util.Optional;
+
 @Repository
 public interface AuthorRepository extends CrudRepository<Author, Long> {
-    Author findAuthorByUsername(String username);
+    Optional<Author> findAuthorByUsername(String username);
+    Boolean existsAuthorByUsername(String username);
 }
