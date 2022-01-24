54975182ad090153d2d31e058b80572ce8af553f
Svyatoslav <sergeyevsvyatoslav@yandex.ru>
2022-01-25 00:31:03 +0300

    renamed some methods

--- src/main/java/tech/sergeyev/linkshortener/service/ShortLinkService.java
+++ src/main/java/tech/sergeyev/linkshortener/service/ShortLinkService.java
@@ -3,9 +3,13 @@ package tech.sergeyev.linkshortener.service;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import tech.sergeyev.linkshortener.persistence.model.Author;
 import tech.sergeyev.linkshortener.persistence.model.ShortLink;
 import tech.sergeyev.linkshortener.persistence.repository.ShortLinkRepository;
+import tech.sergeyev.linkshortener.service.security.AuthorDetailsService;
 
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
@@ -17,17 +21,25 @@ public class ShortLinkService {
 
     final ShortLinkRepository repository;
     final ShortLinkGenerator generator;
+    final AuthorDetailsService authorService;
     @Value("${code.length}")
     int codeLength;
 
     public ShortLinkService(ShortLinkRepository repository,
-                            ShortLinkGenerator generator) {
+                            ShortLinkGenerator generator,
+                            AuthorDetailsService authorService) {
         this.repository = repository;
         this.generator = generator;
+        this.authorService = authorService;
     }
 
     public ShortLink create(String originalUrl) {
         ShortLink link = new ShortLink();
+
+        UserDetails authorizedUser = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        // TODO: юзер не кастуется в автора!!!
+        Author author = (Author) authorService.loadUserByUsername(authorizedUser.getUsername());
+        link.setAuthor(author);
 
         String code = generator.generateCode(codeLength);
         link.setShortCode(code);
@@ -40,7 +52,7 @@ public class ShortLinkService {
         return repository.save(link);
     }
 
-    public ShortLink getByShortCode(String code) {
+    public ShortLink getOriginalUrlByShortCode(String code) {
         return repository.findByShortCode(code);
     }
 }
